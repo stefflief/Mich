7383bdf8cf91916122016247dc415ca6152c23b1
stefflief <159695518+stefflief@users.noreply.github.com>
2024-02-12 19:44:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "I love you this much! 😘";
   gif.src =
-    "<iframe src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnFxcTkycGtud3Axcmh5cndmYmxvbXp5OGF3OGdoYzUzZHRlMzJ4cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/WOOFsm13stf44Lan8c/giphy.gif";
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnFxcTkycGtud3Axcmh5cndmYmxvbXp5OGF3OGdoYzUzZHRlMzJ4cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/WOOFsm13stf44Lan8c/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
